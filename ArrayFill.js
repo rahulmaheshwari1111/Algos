74362bf62deacfc96caf6004e3ce447aa2c1620b
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-03-28 21:28:04 +0530

    simple array fill question

---
+++ ArrayFill.js
@@ -0,0 +1,17 @@
+//array.fill method
+
+//arr.fill(value[, start[, end]])
+
+
+const arra1 = [1,2,3,4,5]
+console.log(arra1.fill(100, 0,2))
+// expected output [100,100,3,4,5]  // firs argument is the number that i want to fill with ... from 0 index to 2nd index
+console.log(arra1)
+//it changes the original array
+// expected output [100,100,3,4,5]
+ 
+
+//const arra1 = [[1,2,3,4,5],[2,3]]
+//expected output [100,100]
+
+// filter
